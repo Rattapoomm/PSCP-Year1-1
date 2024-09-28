cdbf652aa5c71eeb03e9a3d58764016c214781a0
Poomer <poomyakusa1@gmail.com>
2024-09-28 15:09:31 +0700

    new

--- PSCP/Week 8 (Mock Mid 2022)/[Mock Midterm 2022] Palindrome.py
+++ PSCP/Week 8 (Mock Mid 2022)/[Mock Midterm 2022] Palindrome.py
@@ -1,1 +1,18 @@
-'''Palindrome'''
+'''[Midterm 2022] Palindrome'''
+def palindrome(num, time):
+    '''sss'''
+    count = 0
+    while count != num:
+        pari = "%02d" % (int(time[-2:]) + 1)
+        hours = time[0:2].replace(":", "")
+        if int(pari) % 60 == 0 and int(pari) != 0:
+            pari = "00"
+            val_y = int(hours) + 1
+            hours = str(val_y)
+        if int(hours) % 24 == 0:
+            hours = "0"
+        time = hours + ":" + pari
+        if time.replace(":", "") == time.replace(":", "")[::-1]:
+            count += 1
+            print(time)
+palindrome(int(input()), input())
--- PSCP/Week 8 (Mock Mid 2022)/[Mock Midterm 2022] Volleyball.py
+++ PSCP/Week 8 (Mock Mid 2022)/[Mock Midterm 2022] Volleyball.py
@@ -1,1 +1,47 @@
-'''Voll'''
+"""volleyball"""
+
+def comp(awin, bwin, val):
+    """function"""
+    return awin >= val or bwin >= val
+
+def tied(awin, bwin):
+    """function"""
+    return abs(awin - bwin) >= 2
+
+def volley(val, awin=0, bwin=0, teama=0, teamb=0):
+    """function"""
+    string = ""
+    add = 0
+    end_game = False
+    while len(val):
+        for i in val:
+            if (comp(awin, bwin, 25) and tied(awin, bwin) and add <= 3):
+                end_game = True
+                break
+            if (comp(awin, bwin, 15) and tied(awin, bwin) and add == 4):
+                end_game = True
+                break
+            if i == "A":
+                awin += 1
+            elif i == "B":
+                bwin += 1
+            string += i
+        add += 1
+        if awin > bwin:
+            teama += 1
+        else:
+            teamb += 1
+        if add <= 5:
+            print(f"Set {add}: A ({awin}) | B ({bwin})")
+        val = val.replace(string, "", 1)
+        if add >= 4 and end_game and teama - teamb or (abs(teama - teamb) == 3):
+            print(f"A won {teama}:{teamb} set" * (teama > teamb) +
+                  f"B won {teamb}:{teama} set" * (teama < teamb))
+            break
+        end_game = False
+        awin = 0
+        bwin = 0
+        string = ""
+    if not end_game:
+        print("The game has not finished yet.")
+volley(input() + " ")
