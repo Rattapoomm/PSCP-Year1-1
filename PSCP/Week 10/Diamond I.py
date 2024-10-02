afbb926dbdf5ecc5addc7fedb68e8adc9916bf5e
Poomer <poomyakusa1@gmail.com>
2024-10-03 00:44:43 +0700

    s

--- PSCP/Week 10/Diamond I.py
+++ PSCP/Week 10/Diamond I.py
@@ -1,1 +1,20 @@
 '''Dai'''
+def plus(num, per_senten):
+    '''sss'''
+    count = 0
+    total = 3
+    loca = 0
+    ans = []
+    for _ in range(num):
+        txt = input()
+        new = txt.split()
+        ans.extend(new)
+    #for _ in range(num):
+    #print(ans[9])
+    #print(ans[19])
+    #print(ans[29])
+    #while count == total:
+    print(int(ans[loca]))+(int(ans[int(loca)+int(per_senten)]))
+
+
+plus(int(input()), int(input()))
